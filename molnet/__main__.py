91a90cdd3ca4c3828d1e8c500853890822e3f9bc
Lauri Kurki <lauri.v.kurki@gmail.com>
2024-10-30 14:46:13 +0200

    make installable and runnable

---
+++ molnet/__main__.py
@@ -0,0 +1,10 @@
+"""Main file for running the training pipeline."""
+
+from absl import app
+
+def main(argv):
+    del argv  # Unused.
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    app.run(main)
