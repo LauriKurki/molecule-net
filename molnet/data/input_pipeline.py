a1a6dc51a84930624a8d2a6e4f471af40de1f8a6
Lauri Kurki <lauri.v.kurki@gmail.com>
2024-11-01 14:05:21 +0200

    addd input pipeline

---
+++ molnet/data/input_pipeline.py
@@ -0,0 +1,82 @@
+import os
+import re
+
+from absl import logging
+
+import tensorflow as tf
+
+import chex
+import ml_collections
+
+from typing import Dict, List, Sequence
+
+def get_datasets(
+    rng: chex.PRNGKey,
+    config: ml_collections.ConfigDict,
+) -> Dict[str, tf.data.Dataset]:
+    """Loads datasets for each split."""
+
+    filenames = sorted(os.listdir(config.root_dir))
+    filenames = [
+        os.path.join(config.root_dir, f)
+        for f in filenames
+        if f.startswith("maps_")
+    ]
+
+    if len(filenames) == 0:
+        raise ValueError(f"No files found in {config.root_dir}.")
+    
+    # Partition the filenames into train, val, and test.
+    def filter_by_molecule_number(
+        filenames: Sequence[str], start: int, end: int
+    ) -> List[str]:
+        def filter_file(filename: str, start: int, end: int) -> bool:
+            filename = os.path.basename(filename)
+            file_start, file_end = [int(val) for val in re.findall(r"\d+", filename)]
+            return start <= file_start and file_end <= end
+
+        return [f for f in filenames if filter_file(f, start, end)]
+
+    # Number of molecules for training can be smaller than the chunk size.
+    files_by_split = {
+        "train": filter_by_molecule_number(filenames, *config.train_molecules),
+        "val": filter_by_molecule_number(filenames, *config.val_molecules),
+        "test": filter_by_molecule_number(filenames, *config.test_molecules),
+    }
+
+    element_spec = tf.data.Dataset.load(filenames[0]).element_spec
+    datasets = {}
+    for split, files_split in files_by_split.items():
+
+        dataset_split = tf.data.Dataset.from_tensor_slices(files_split)
+        dataset_split = dataset_split.interleave(
+            lambda path: tf.data.Dataset.load(path, element_spec=element_spec),
+            num_parallel_calls=tf.data.AUTOTUNE,
+            deterministic=True,
+        )
+
+        # Shuffle the dataset.
+        if config.shuffle_datasets:
+            dataset_split = dataset_split.shuffle(1000, seed=config.rng_seed)
+
+        # Repeat the dataset.
+        dataset_split = dataset_split.repeat()
+
+        # batches consist of a dict {'images': image, 'xyz': xyz, 'atom_map': atom_map}
+        # pad xyz with zeros, its shape is [num_atoms, 5] - pad to [max_atoms, 5]
+        dataset_split = dataset_split.map(
+            lambda x: {
+                "images": x["images"],
+                "xyz": tf.pad(x["xyz"], [[0, config.max_atoms - tf.shape(x["xyz"])[0]], [0, 0]]),
+                "atom_map": x["atom_map"],
+            },
+            num_parallel_calls=tf.data.AUTOTUNE,
+            deterministic=True,
+        )
+
+        # Batch the dataset.
+        dataset_split = dataset_split.batch(config.batch_size)
+        dataset_split = dataset_split.prefetch(tf.data.AUTOTUNE).as_numpy_iterator()
+        
+        datasets[split] = dataset_split
+    return datasets
---
+++ tests/input_test.py
@@ -0,0 +1,48 @@
+import jax
+import ml_collections
+import time
+
+from absl.testing import absltest
+
+from molnet.data import input_pipeline
+from configs import root_dirs, default
+
+def get_config():
+    config = default.get_config()
+    #config.root_dir = root_dirs.get_root_dir()
+    config.root_dir = '/l/data/molnet/atom_maps'
+
+    config.batch_size = 16
+    config.train_molecules = (0, 1000)
+    config.val_molecules = (1000, 2000)
+    config.test_molecules = (2000, 3000)
+
+    return config
+
+
+class TestInputPipeline(absltest.TestCase):
+    def test_input_pipeline(self):
+        config = get_config()
+
+        print(config)
+
+        rng = jax.random.PRNGKey(0)
+        datarng, rng = jax.random.split(rng)
+        datasets = input_pipeline.get_datasets(datarng, config)
+
+        trainset = datasets["train"]
+
+        for i in range(10):
+
+            t0 = time.perf_counter()
+            batch = next(trainset)
+            image = batch["images"]
+            atom_map = batch["atom_map"]
+            xyz = batch["xyz"]
+
+            print(f"Batch {i}: {image.shape}, {atom_map.shape}, {xyz.shape}")
+            print(f"Time taken: {(time.perf_counter() - t0)*1000:.2f} ms")
+
+
+if __name__ == '__main__':
+    absltest.main()
