339af1f2cf809cf08621b6fb1409415d64270aed
Lauri Kurki <lauri.v.kurki@gmail.com>
2024-11-15 19:54:01 +0900

    updated tests

--- torch_tests/train_test.py
+++ torch_tests/train_test.py
@@ -4,7 +4,7 @@ import os
 import torch
 torch.set_float32_matmul_precision('medium')
 
-from configs.tests import torch_attention_test
+from configs.tests import torch_attention_test, equiv_attn_unet_test, equiv_unet_test
 from molnet import train_torch as train
 from configs import root_dirs
 
@@ -14,12 +14,14 @@ from absl import logging
 ALL_CONFIGS = {
     #"unet": unet_test.get_config(),
     "attention": torch_attention_test.get_config(),
+    "equiv_attention": equiv_attn_unet_test.get_config(),
+    "equiv_unet": equiv_unet_test.get_config(),
 }
 
 class TrainTest(parameterized.TestCase):
 
     @parameterized.product(
-        config_name=list(ALL_CONFIGS.keys())
+        config_name=["equiv_unet"],
     )
     def test_train(
         self,
@@ -28,6 +30,7 @@ class TrainTest(parameterized.TestCase):
 
         # Check if CUDA is available
         logging.info(f"Training on cuda: {torch.cuda.is_available()} -- {torch.cuda.device_count()} devices.")
+        logging.info(f"Training on mps: {torch.mps.is_available()} -- {torch.mps.device_count()} devices.")
 
         # Get the config
         config = ALL_CONFIGS[config_name]
