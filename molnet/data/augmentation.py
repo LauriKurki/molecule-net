rotation augmentation fix

molnet/data/augmentation.py
@@ -96,7 +96,7 @@ def random_rotate_3d_stacks_with_coords(x, xyz):
         shifted_xy = coords[:, :2] - xy_mean
 
         # Rotate the zero-mean coordinates
-        rotated_xy = tf.matmul(shifted_xy, rotation_matrix)
+        rotated_xy = tf.matmul(shifted_xy, rotation_matrix, transpose_b=True)
 
         # Shift back to the original position
         rotated_xy = rotated_xy + xy_mean

molnet/data/input_pipeline_online.py
@@ -141,8 +141,7 @@ def _preprocess_images(
         x = x + tf.random.normal(tf.shape(x), stddev=noise_std)
 
     # Apply rotation and flip augmentation.
-    #x, xyz = augmentation.random_rotate_3d_stacks_with_coords(x, xyz)
-    #x, xyz = augmentation.random_flip_3d_stacks_with_coords(x, xyz, x.shape[:-1])
+    x, shifted_xyz = augmentation.random_rotate_3d_stacks_with_coords(x, shifted_xyz)
 
     # Create cutout augmentation.
     x = augmentation.add_random_cutouts(x, cutout_probs=cutout_probs, cutout_size_range=(5, 10))