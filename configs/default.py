14c1cf49acc53446d4ed1ecd512955171cf16e54
Lauri Kurki <lauri.v.kurki@gmail.com>
2025-01-08 13:38:27 +0200

    small changes to input test and default config

--- configs/default.py
+++ configs/default.py
@@ -11,9 +11,9 @@ def get_config() -> ml_collections.ConfigDict:
     config.rng_seed = 0
     config.noise_std = 0.1
     config.sigma = 0.2
-    config.z_cutoff = 2.0
+    config.z_cutoff = 1.0
     config.gaussian_factor = 1.0
-    config.interpolate_input_z = 20
+    config.interpolate_input_z = None
     config.cutout_probs = [0.5, 0.3, 0.1, 0.05, 0.05]
     config.max_shift_per_slice = 0.02
     config.max_atoms = 54
