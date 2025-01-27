94145a2f8503637b32c7b676d30e2e2173351803
Lauri Kurki <lauri.v.kurki@gmail.com>
2025-01-27 13:40:30 +0200

    updates to water pipeline

--- molnet/data/input_pipeline_water.py
+++ molnet/data/input_pipeline_water.py
@@ -80,11 +80,8 @@ def get_datasets(
         dataset_split = dataset_split.map(
             lambda x: _preprocess_batch(
                 x,
-                config.noise_std,
                 interpolate_z=config.interpolate_input_z,
                 z_cutoff=config.z_cutoff,
-                cutout_probs=config.cutout_probs,
-                max_shift_per_slice=config.max_shift_per_slice,
             ),
             num_parallel_calls=tf.data.AUTOTUNE,
             deterministic=True,
@@ -107,6 +104,9 @@ def get_datasets(
             lambda x: _augment_image_and_atom_map(
                 x,
                 crop_size=128, # TODO: Make this a parameter in the config.
+                cutout_probs=config.cutout_probs,
+                noise_std=config.noise_std,
+                max_shift_per_slice=config.max_shift_per_slice,
             ),
             num_parallel_calls=tf.data.AUTOTUNE,
             deterministic=True,
@@ -122,11 +122,8 @@ def get_datasets(
 
 def _preprocess_batch(
     batch: Dict[str, tf.Tensor],
-    noise_std: float = 0.0,
     interpolate_z: Optional[int] = None,
     z_cutoff: float = 1.0,
-    cutout_probs: Optional[List[float]] = [0.5, 0.3, 0.1, 0.05, 0.05],
-    max_shift_per_slice: float = 0.02,
 ) -> Dict[str, tf.Tensor]:
     """Preprocesses images."""
     
@@ -160,21 +157,6 @@ def _preprocess_batch(
     # Interpolate to `interpolate_z` z slices
     if interpolate_z is not None:
         x = tf.image.resize(x, (x.shape[1], interpolate_z), method='bilinear')
-
-    # Randomly shift the slices.
-    x = augmentation.random_slice_shift(x, max_shift_per_slice=max_shift_per_slice)
-
-    # Rotate inputs tensor and coordinates.
-    # Done elsewhere in the pipeline.
-
-    # Normalize the images to zero mean and unit variance.
-    # Water images are already normalized.
-
-    # Add noise to the images.
-    x = augmentation.add_noise(x, noise_std)
-
-    # Create cutouts.
-    x = augmentation.add_random_cutouts(x, cutout_probs=cutout_probs, cutout_size_range=(2, 10))
 
     sample = {
         "images": x,
@@ -251,6 +233,9 @@ def _compute_atom_maps(
 def _augment_image_and_atom_map(
     batch: Dict[str, tf.Tensor],
     crop_size: int,
+    cutout_probs: List[float],
+    noise_std: float,
+    max_shift_per_slice: float,
 ) -> Dict[str, tf.Tensor]:
     """Crops images and atom maps."""
     x = batch["images"]
@@ -259,8 +244,19 @@ def _augment_image_and_atom_map(
     # Random rotation
     x, y = augmentation.random_rotate_image_and_atom_map(x, y)
 
+    # Randomly shift the slices.
+    x = augmentation.random_slice_shift(x, max_shift_per_slice=max_shift_per_slice)
+
     # Crop the images and atom maps.
     x, y = augmentation.random_crop(x, y, crop_size)
+
+    # Add noise to the images.
+    x = augmentation.add_noise(x, noise_std)
+
+    # Create cutouts.
+    x = augmentation.add_random_cutouts(
+        x, cutout_probs=cutout_probs, cutout_size_range=(2, 10), image_size=crop_size
+    )
 
     return {
         "images": x,
@@ -268,4 +264,3 @@ def _augment_image_and_atom_map(
         "xyz": batch["xyz"],
         "sw": batch["sw"],
     }
-    
