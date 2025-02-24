4b3ea917cf4ae39e80d03ca351c83d692e473dd7
Lauri Kurki <lauri.v.kurki@gmail.com>
2025-02-24 14:28:30 +0200

    typing hint for center cropping

--- molnet/data/augmentation.py
+++ molnet/data/augmentation.py
@@ -57,7 +57,8 @@ def center_crop(
     x: tf.Tensor,
     y: tf.Tensor,
     size: int,
-    shift: int = 0):
+    shift: int = 0
+) -> Tuple[tf.Tensor, tf.Tensor]:
     """
     Center-crops a stack of 2D images to the specified size.
 
